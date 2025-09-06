8f74ca1b9980b509b3fcaed287436f021819b3f4
thewgroupbr <tyler@thewgroupbr.com>
2025-09-06 08:55:40 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 // server.js
 const express = require("express");
+const routes = require("./routes");
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (_req, res) => res.send("W Group API is live 🚀"));
+app.use(routes);
 
-app.listen(port, () => {
-  console.log(`API running on port ${port}`);
-});
+app.listen(port, () => console.log(`API running on port ${port}`));
